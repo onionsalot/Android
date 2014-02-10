4c2881de91feef7eef9dd1dc1a503cde5a3ed173
superonion <onionsinmypants@gmail.com>
2014-02-10 12:06:59 -0500

    Project 1 commit

--- firstapp/src/com/example/firstapp/MainActivity.java
+++ firstapp/src/com/example/firstapp/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.firstapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button b = (Button) findViewById(R.id.button1);
+        b.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, second.class));
+
+			}
+		});
     }
 
 
---
+++ project1/src/com/example/project1/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.project1;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_main);
+	        final EditText et = (EditText) findViewById(R.id.input_text);
+	        final TextView tv = (TextView) findViewById(R.id.output_text);
+	        final Button button = (Button) findViewById(R.id.button1);
+	        button.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+	                String boob = et.getText().toString();
+	                tv.setText(boob);
+
+					
+				}
+			});
+	    }
+	    
+
+	}
+
